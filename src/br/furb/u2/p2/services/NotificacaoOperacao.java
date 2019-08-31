5bb73d1b83fd276869c7ec3a739f625c6701e621
Caetano Siemann <caetanosiemann@gmail.com>
2019-08-30 21:33:59 -0300

    Update NotificacaoOperacao.java

--- src/br/furb/u2/p2/services/NotificacaoOperacao.java
+++ src/br/furb/u2/p2/services/NotificacaoOperacao.java
@@ -2,17 +2,26 @@ package br.furb.u2.p2.services;
 
 import java.util.Observable;
 
+import br.furb.u2.p2.models.ContaCorrente;
+
 public class NotificacaoOperacao implements Servico{
 
+	Observable contaCorrente;
+	
+	public NotificacaoOperacao(Observable contaCorrente) {
+		this.contaCorrente = contaCorrente;
+		contaCorrente.addObserver(this);
+	}
+	
 	@Override
 	public void update(Observable o, Object arg) {
-		// TODO Auto-generated method stub
-		
+		if (o instanceof ContaCorrente) {
+			executar(((ContaCorrente) o).operacoes.);
+		}
 	}
 
 	@Override
 	public void executar(Operacao op) {
-		// TODO Auto-generated method stub
 		
 	}
 	
